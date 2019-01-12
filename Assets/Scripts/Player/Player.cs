8a3245751f10863f265d687e16f63ba28d837cbf
Vincent <vincent.guillemette1@gmail.com>
2019-01-12 11:35:43 -0500

    Changements au prefab du bateau

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
-	public PlayerId playerId;
-	public PlayerHealth playerHealth;
-	public PlayerHealthBar playerHealthBar;
-	public PlayerActions playerActions;
-	public PlayerSounds playerSounds;
-	public AudioSource audioSource;
+    public PlayerId playerId;
+    public PlayerHealth playerHealth;
+    public PlayerHealthBar playerHealthBar;
+    public PlayerActions playerActions;
+    public PlayerSounds playerSounds;
+    public AudioSource audioSource;
+    public List<GameObject> leftCannons;
+    public List<GameObject> rightCannons;
 
     // Use this for initialization
     void Start () {
